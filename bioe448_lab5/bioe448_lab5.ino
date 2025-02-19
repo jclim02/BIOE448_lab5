69c1db27710f5d95afe8e44abedf7a86652cb859
jclim02 <janie@neurastasis.com>
2025-02-19 13:30:13 -0600

    BLE setup

--- bioe448_lab5/bioe448_lab5.ino
+++ bioe448_lab5/bioe448_lab5.ino
@@ -1,7 +1,7 @@
-// #include <ArduinoBLE.h>
-// BLEService newService("180A"); // creating the service
-// BLEByteCharacteristic readChar("2A57", BLERead);
-// BLEByteCharacteristic writeChar("2A58", BLEWrite);
+#include <ArduinoBLE.h>
+BLEService newService("180A"); // creating the service
+BLEByteCharacteristic readChar("2A57", BLERead);
+BLEByteCharacteristic writeChar("2A58", BLEWrite);
 
 const int trigPin = 8;
 const int echoPin = 10;
@@ -20,23 +20,23 @@ void setup() {
 
   Serial.begin(9600);
 
-  // while(!Serial);
-  // if (!BLE.begin()){
-  //   Serial.println("Waiting for ArduinoBLE");
-  //   while(1);
-  // }
+  while(!Serial);
+  if (!BLE.begin()){
+    Serial.println("Waiting for ArduinoBLE");
+    while(1);
+  }
 
-  // BLE.setDeviceName("Your names here");
-  // BLE.setAdvertisedService(newService);
-  // newService.addCharacteristic(readChar);
-  // newService.addCharacteristic(writeChar);
-  // BLE.addService(newService);
+  BLE.setDeviceName("VJP");
+  BLE.setAdvertisedService(newService);
+  newService.addCharacteristic(readChar);
+  newService.addCharacteristic(writeChar);
+  BLE.addService(newService);
 
-  // readChar.writeValue(0);
-  // writeChar.writeValue(0);
+  readChar.writeValue(0);
+  writeChar.writeValue(0);
 
-  // BLE.advertise();
-  // Serial.println("Bluetooth device active");
+  BLE.advertise();
+  Serial.println("Bluetooth device active");
 
 }
 
