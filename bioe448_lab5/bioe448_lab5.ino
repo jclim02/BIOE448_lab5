999f3cbce0be393e0c3f0a3fcbf574f3943bd68b
jclim02 <janie@neurastasis.com>
2025-02-19 14:24:12 -0600

    Writing to the device to read

--- bioe448_lab5/bioe448_lab5.ino
+++ bioe448_lab5/bioe448_lab5.ino
@@ -1,7 +1,10 @@
 #include <ArduinoBLE.h>
-BLEService newService("180A"); // creating the service
-BLEByteCharacteristic readChar("2A57", BLERead);
-BLEByteCharacteristic writeChar("2A58", BLEWrite);
+BLEService newService("180A");
+
+BLEByteCharacteristic readChar("2A58", BLERead);
+BLEByteCharacteristic writeChar("2A57", BLEWrite);
+
+long previousMillis = 0;
 
 const int trigPin = 8;
 const int echoPin = 10;
@@ -9,15 +12,8 @@ long duration;
 int distanceCm, distanceInch;
 
 void setup() {
-
-  // peripheral pins
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-
-  // LED pins
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-
   Serial.begin(9600);
 
   while(!Serial);
@@ -31,52 +27,51 @@ void setup() {
   newService.addCharacteristic(readChar);
   newService.addCharacteristic(writeChar);
   BLE.addService(newService);
-
+  
   readChar.writeValue(0);
   writeChar.writeValue(0);
 
   BLE.advertise();
   Serial.println("Bluetooth device active");
-
 }
 
 void loop() {
 
-  // Sending trigger pulse
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2); // good practice to send low state to peripheral first
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  BLEDevice central = BLE.central(); // wait for a BLE central
 
-  duration = pulseIn(echoPin, HIGH);
+  if (central) {  // if a central is connected to the peripheral
+    Serial.print("Connected to central: ");
+    
+    Serial.println(central.address()); // print the central's BT address
+    
+    digitalWrite(LED_BUILTIN, HIGH); // turn on the LED to indicate the connection
 
-  // Calibrating sensor
-  distanceCm = duration / 58;
-  distanceInch = duration / 148;
-  Serial.print("Distance: ");
-  Serial.print(distanceCm);
-  Serial.print(" cm/");
-  Serial.print(distanceInch);
-  Serial.println(" in");
+    // while (central.connected()) { // while the central is connected:
+      long currentMillis = millis();
+      
+      // if (currentMillis - previousMillis >= 200) { 
+        previousMillis = currentMillis;
 
-  // //LED feedback for distance
-  //   if (distanceInch > 10) {
-  //   digitalWrite(3, HIGH);
-  //   digitalWrite(4, LOW);
-  // } 
-  // else {
-  //   digitalWrite(4, HIGH);
-  //   digitalWrite(3, LOW);   
-  // }
+        digitalWrite(trigPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin, LOW);
 
-  delay(1000);
+        duration = pulseIn(echoPin, HIGH);
+        distanceCm = duration * 0.034 / 2;
 
-  readChar.writeValue(distanceCm);
+        Serial.print("Distance: ");
+        Serial.print(distanceCm);
+        Serial.println(" cm");
 
-  // if (writeChar.written()) {
-  //   if (writeChar.value()) {
-  //   }
-  // }
- 
+        delay(100);
+        readChar.writeValue(distanceCm);
+        Serial.println("Distance printed to peripheral");
+      // }
+    // }
+
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
 }
